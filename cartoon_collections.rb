2c1f708a86c317728c54d0dd29e090ff87813f8c
Sierra McCahon <smitche4@mail.umw.edu>
2018-04-01 01:25:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,13 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+   i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
 
 def find_the_cheese(array)
